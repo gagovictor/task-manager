bea43ec9bb7ff761b8c683ed41f2cbd855b52be6
Victor Gago <victorgago-work@outlook.com>
2024-08-01 16:26:40 -0300

    JWT fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,29 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 const authMiddleware = async (req, res, next) => {
+  console.log(req.headers.authorization);
   const token = req.headers.authorization?.split(' ')[1];
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findByPk(decoded.id);
-      if (!req.user) {
-        return res.status(401).json({ error: 'Unauthorized - user not found' });
-      }
-      next();
-    } catch (err) {
-      console.error('Error verifying token:', err);
-      res.status(401).json({ error: 'Unauthorized' });
+  
+  if (!token) {
+    console.error('No authorization token provided');
+    return res.status(401).json({ error: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findByPk(decoded.userId);
+    if (!req.user) {
+      console.error('User not found for userId:', decoded.id);
+      return res.status(401).json({ error: 'Unauthorized - user not found' });
     }
-  } else {
-    res.status(401).json({ error: 'No token provided' });
+    
+    console.log('User found:', req.user);
+    next();
+  } catch (err) {
+    console.error('Error verifying token:', err.message);
+    res.status(401).json({ error: 'Unauthorized' });
   }
 };
 
