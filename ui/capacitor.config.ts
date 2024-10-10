d9f51488c0a735cd16e7e6489ff3b535ffa412cb
Victor Gago <victorgago-work@outlook.com>
2024-10-10 16:36:53 -0300

    Capacitor support for Android (tested), iOS (pending tests)

---
+++ ui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'works.gago.tasks',
+  appName: 'Task Manager',
+  webDir: 'build'
+};
+
+export default config;
